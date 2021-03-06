d5c6300a037b5d3eaecdddfd74b88abaeb018794
SamarthBrownHatCoder <78861159+SamarthBrownHatCoder@users.noreply.github.com>
2021-03-06 20:38:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -92,7 +92,7 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-130) {
+    if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-100) {
       jump.play( )
       trex.velocityY = -12;
       touches = [];
